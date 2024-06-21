065e5d07a03ea4b5b5f91484236e1568c30a90c0
horiaomar25 <horiaomar6@gmail.com>
2024-06-21 17:16:35 +0100

    added: new

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  @vite('resources/css/app.css', 'resources/js/app.js')
+
+
 </head>
 <body>
 <div class="navbar bg-base-100 border border-black ">
